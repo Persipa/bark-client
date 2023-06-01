7d410da757dbf2ac97e83121f1c8deae21dfd006
Persipa <khxu10234@icloud.com>
2023-06-01 10:24:22 +0800

    feat(config): 新增dubbo config

---
+++ bark-client-core/src/main/java/site/persipa/bark/common/config/DubboConfiguration.java
@@ -0,0 +1,12 @@
+package site.persipa.bark.common.config;
+
+import org.apache.dubbo.config.spring.context.annotation.EnableDubbo;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author persipa
+ */
+@Configuration
+@EnableDubbo
+public class DubboConfiguration {
+}
